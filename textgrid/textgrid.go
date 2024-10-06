212cc5f750c22ec6866bc12166a7e9c73922b797
vocatart <conemusicproductions@gmail.com>
2024-10-06 14:00:52 -0600

    parsing tg to slice

--- textgrid/textgrid.go
+++ textgrid/textgrid.go
@@ -3,6 +3,7 @@ package textgrid
 import (
 	"log"
 	"os"
+	"strings"
 )
 
 type TextGrid struct {
@@ -42,17 +43,70 @@ func (tg *TextGrid) SetName(name string) {
 	tg.name = name
 }
 
+// Gets tiers of TextGrid
 func (tg TextGrid) GetTiers() []Tier {
 	return tg.tiers
 }
 
+// Takes a path to a .TextGrid file and reads its contents into a TextGrid.
 func ReadTextgrid(path string) TextGrid {
 	tg := TextGrid{}
 
-	tgData, err := os.Open(path)
+	// check if the file exists
+	// reading into memory is alot easier than line-by-line with textgrids
+	tgData, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer tgData.Close()
 
+	tgContent := processContent(tgData)
+
+	if !verifyHead(tgContent) {
+		log.Fatal("malformed textgrid head!")
+	}
+
+	return tg
+
+}
+
+// turns textgrid file content into a slice of useable strings
+func processContent(data []byte) []string {
+	// remove all empty lines
+	tgString := strings.ReplaceAll(string(data), "\n\n", "\n")
+
+	var sanitized strings.Builder
+	inQuotes := false
+
+	// remove all spaces unless the space is between quotations (part of a label)
+	for i := 0; i < len(tgString); i++ {
+		char := tgString[i]
+
+		if char == '"' {
+			inQuotes = !inQuotes
+		}
+
+		if inQuotes || char != ' ' {
+			sanitized.WriteByte(char)
+		}
+	}
+
+	return strings.Split(sanitized.String(), "\n")
+}
+
+// verifies the existence and content of FileType and ObjectClass
+func verifyHead(tgContent []string) bool {
+	fileType := tgContent[0]
+	objectClass := tgContent[1]
+
+	if !strings.Contains(fileType, "ooTextFile") {
+		log.Println("malformed fileType")
+		return false
+	}
+
+	if !strings.Contains(objectClass, "TextGrid") {
+		log.Println("malformed objectClass")
+		return false
+	}
+
+	return true
 }
---
+++ textgrid/textgrid_test.go
@@ -0,0 +1,7 @@
+package textgrid
+
+import "testing"
+
+func TestReadingTextgrid(t *testing.T) {
+	ReadTextgrid("examples/default_bad_romance.TextGrid")
+}
